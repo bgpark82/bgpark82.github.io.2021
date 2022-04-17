20604dbefc77e9e565f900750c71d5d5e413175d
bgpark82 <bgpark82@gmail.com>
2022-04-17 23:24:16 +0900

    refactor: modify static content

--- java/src/test/java/bgpark/static_block/StaticTest.java
+++ java/src/test/java/bgpark/static_block/StaticTest.java
@@ -2,12 +2,40 @@ package bgpark.static_block;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StaticTest {
 
     @Test
     void outOfMemory() {
         for (int i = 0; i < 10; i++) {
             new StaticBlock();
+        }
+    }
+
+    @Test
+    void classVariable() {
+        Person p1 = new Person();
+        Person p2 = new Person();
+        assertThat(p1.name.hashCode() == p2.name.hashCode()).isTrue();
+    }
+
+    @Test
+    void staticFinal() {
+        System.out.println(Car.name);
+    }
+
+    static class Person {
+
+        public static String name = "name";
+    }
+
+    static class Car {
+
+        static final String name;
+
+        static {
+            name = "car";
         }
     }
 }
